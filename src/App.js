4186224836f9117f07948e1007bd325931f3d610
compiere <compiere@localhost.localdomain>
2019-12-02 17:09:25 +0100

    app with hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name : 'Max', age: 28 },
+      { name : 'Greg', age: 27 },
+      { name : 'Steph', age: 26 }
+  ],
+  otherState: 'Some other state not used here'
+});
+
+console.log(personsState);
+
+const switchNameHandler = () => {
+  setPersonsState({persons: [
+    { name : 'Maximilian', age: 28 },
+    { name : 'Greg', age: 27 },
+    { name : 'Steph', age: 26 }
+    ]
+  });
+};
+ 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really Working</p>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App !!!'));
   }
-}
 
-export default App;
+export default app;
+
+
+
+// state = {
+//   persons: [
+//     { name : 'Max', age: 28 },
+//     { name : 'Greg', age: 27 },
+//     { name : 'Steph', age: 26 }
+//   ],
+//   otherState: 'Some other state'
+//  }
+
+//  switchNameHandler = () => {
+//   //  console.log('Was clicked');
+//   this.setState({persons: [
+//     { name : 'Maximilian', age: 28 },
+//     { name : 'Greg', age: 27 },
+//     { name : 'Steph', age: 26 }
+//   ]});
+//  }
