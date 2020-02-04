713150ee13ad767c155ca8faa5789e00709a46eb
Peterschmitt <gregoire.peterschmitt@gmail.com>
2020-02-04 17:10:17 +0100

    context for class-based components, and end on demo project (persons manager)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.css'
 import AuthContext from '../../context/auth-context';
 
 
 const cockpit = (props) => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+  console.log(authContext.authenticated);
 
   useEffect(() => {
     console.log('[Cockpit.js] use Effect');
@@ -49,9 +51,7 @@ const cockpit = (props) => {
               className={btnClass}
               onClick={props.clicked}>Toggle persons
             </button>
-            <AuthContext.Consumer>
-              {(context) => (<button onClick={context.login}>Log in</button>)}
-            </AuthContext.Consumer>
+            <button onClick={authContext.login}>Log in</button>
         </div>
     );
 
